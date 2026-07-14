52c391c849b65e02ac6702ca38727ea0a1b11feb
kafka farel <kafkafj1@gmail.com>
2026-07-15 00:24:39 +0700

    mengubah penamaan redis password

--- src/infrastruktur/queue/queue.module.ts
+++ src/infrastruktur/queue/queue.module.ts
@@ -8,7 +8,7 @@ import { BullmqModule } from './bullmq/bullmq.module';
       connection: {
         host: process.env.REDIS_HOST || 'redis',
         port: Number(process.env.REDIS_PORT || 6379),
-        password: process.env.REDIS_PASS || undefined,
+        password: process.env.REDIS_PASSWORD || undefined,
       },
     }),
 
--- src/infrastruktur/redis/redis.service.ts
+++ src/infrastruktur/redis/redis.service.ts
@@ -16,7 +16,7 @@ export class RedisService implements OnModuleDestroy {
       : new Redis({
           host: process.env.REDIS_HOST ?? 'localhost',
           port: Number(process.env.REDIS_PORT ?? 6379),
-          password: process.env.REDIS_PASS || undefined,
+          password: process.env.REDIS_PASSWORD || undefined,
           db: Number(process.env.REDIS_DB ?? 0),
 
           retryStrategy: (times) => Math.min(times * 50, 2000),
