11f0feb1ba98705731561af5e8bebbdba7b1cfdf
kafka farel <kafkafj1@gmail.com>
2026-07-14 23:33:08 +0700

    menghapus validation pape di main

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http.exception.filter';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptors';
-import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
